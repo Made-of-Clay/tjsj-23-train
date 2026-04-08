9d7c3d751a31ba6b8c206f7711506faac28e8560
Adam Leis <adam.m.leis@gmail.com>
2026-04-07 20:19:48 -0400

    update helpers and hide grid helper

--- src/addHelpers.ts
+++ src/addHelpers.ts
@@ -1,19 +1,20 @@
 import { AxesHelper, GridHelper } from "three";
-import { getScene } from "./getScene";
 import { getGui } from "./getGui";
+import { getScene } from "./getScene";
 
 export function addHelpers() {
-	const scene = getScene();
-	const gui = getGui();
+    const scene = getScene();
+    const gui = getGui();
+    const helpersFolder = gui.addFolder("Helpers");
 
-	const axesHelper = new AxesHelper(4);
-	axesHelper.visible = false;
-	scene.add(axesHelper);
+    const axesHelper = new AxesHelper(4);
+    axesHelper.visible = false;
+    scene.add(axesHelper);
+    helpersFolder.add(axesHelper, "visible").name("axes");
 
-	const gridHelper = new GridHelper(20, 20, "teal", "darkgray");
-	gridHelper.position.y = -0.01;
-	scene.add(gridHelper);
-
-	const helpersFolder = gui.addFolder("Helpers");
-	helpersFolder.add(axesHelper, "visible").name("axes");
+    const gridHelper = new GridHelper(20, 20, "teal", "darkgray");
+    gridHelper.position.y = -0.01;
+    gridHelper.visible = false;
+    scene.add(gridHelper);
+    helpersFolder.add(gridHelper, "visible").name("grid");
 }
