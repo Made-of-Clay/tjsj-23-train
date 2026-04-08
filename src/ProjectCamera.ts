9c8b0dbfa37b49997547b24899aefe35966cde13
Adam Leis <adam.m.leis@gmail.com>
2026-04-08 11:07:35 -0400

    change project camera from perspective to ortho

--- src/ProjectCamera.ts
+++ src/ProjectCamera.ts
@@ -1,45 +1,82 @@
-import { PerspectiveCamera, type WebGLRenderer } from "three";
-import { OrbitControls } from "three/examples/jsm/Addons.js";
+import type GUI from "lil-gui";
+import { OrthographicCamera, type WebGLRenderer } from "three";
+import { getGui } from "./getGui";
 import { resizeRendererToDisplaySize } from "./helpers/responsiveness";
 
-export function addCamera(canvas: HTMLCanvasElement) {
-    const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
-    camera.position.set(2, 2, 5);
-
-    const cameraControls = new OrbitControls(camera, canvas);
-    cameraControls.enableDamping = true;
-    cameraControls.autoRotate = false;
-    cameraControls.update();
-
-    // const gui = getGui();
-    // might add camera controls to set position better for each spot
-    // const cameraFolder = gui.addFolder("Camera");
-}
-
 export class ProjectCamera {
-    instance: PerspectiveCamera;
+    instance: OrthographicCamera;
     #canvas: HTMLCanvasElement;
-    #cameraControls: OrbitControls;
-    // #cameraFolder: GUI;
+    zoom: number = 1;
+    #cameraFolder: GUI;
 
     constructor(canvas: HTMLCanvasElement) {
-        this.#canvas = canvas;
-        this.instance = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
-        this.instance.position.set(2, 2.5, 5.25);
-        this.#cameraControls = new OrbitControls(this.instance, canvas);
-        this.#cameraControls.enableDamping = true;
+        const gui = getGui();
+        this.#cameraFolder = gui.addFolder("Camera");
 
-        // const gui = getGui();
-        // might add camera controls to set position better for each spot
-        // this.#cameraFolder = gui.addFolder("Camera");
+        this.#canvas = canvas;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const aspect = width / height;
+        const viewHeight = 5;
+        const viewWidth = viewHeight * aspect;
+        this.instance = new OrthographicCamera(
+            -viewWidth / 2,
+            viewWidth / 2,
+            viewHeight / 2,
+            -viewHeight / 2,
+            0.1,
+            1000,
+        );
+        this.instance.position.set(6, 4, 6);
+        this.instance.lookAt(0, 0, 0);
+
+        this.#addGuiControls();
+        this.#updateCameraZoom();
+        this.#handleEvents();
+    }
+
+    #addGuiControls() {
+        const lookAtCenter = () => this.instance.lookAt(0, 0, 0);
+        this.#cameraFolder.add(this.instance.position, "x", -10, 10, 0.1).onChange(lookAtCenter);
+        this.#cameraFolder.add(this.instance.position, "y", -10, 10, 0.1).onChange(lookAtCenter);
+        this.#cameraFolder.add(this.instance.position, "z", -10, 10, 0.1).onChange(lookAtCenter);
+
+        this.#cameraFolder
+            .add(this, "zoom", 0.5, 5, 0.1)
+            .name("Zoom")
+            .onChange(() => {
+                this.#updateCameraZoom();
+            });
+    }
+
+    #handleEvents() {
+        this.#canvas.addEventListener(
+            "wheel",
+            (event) => {
+                event.preventDefault();
+                const zoomSpeed = 0.1;
+                const direction = event.deltaY > 0 ? 1 : -1;
+                this.zoom = Math.max(0.5, Math.min(5, this.zoom + direction * zoomSpeed));
+                this.#updateCameraZoom();
+            },
+            { passive: false },
+        );
+    }
+
+    #updateCameraZoom() {
+        const aspect = this.#canvas.clientWidth / this.#canvas.clientHeight;
+        const viewHeight = 5 / this.zoom;
+        const viewWidth = viewHeight * aspect;
+        this.instance.left = -viewWidth / 2;
+        this.instance.right = viewWidth / 2;
+        this.instance.top = viewHeight / 2;
+        this.instance.bottom = -viewHeight / 2;
+        this.instance.updateProjectionMatrix();
     }
 
     tick(renderer: WebGLRenderer) {
         if (resizeRendererToDisplaySize(renderer)) {
-            this.instance.aspect = this.#canvas.clientWidth / this.#canvas.clientHeight;
-            this.instance.updateProjectionMatrix();
+            this.#updateCameraZoom();
         }
-
-        this.#cameraControls.update();
     }
 }
